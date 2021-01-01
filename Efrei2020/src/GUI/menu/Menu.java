dd69556b69a33d567e0ebae51045b8c7ba7d6967
tomga <tom.gaultier@efrei.net>
2021-01-01 18:42:02 +0100

    menu!!

---
+++ Efrei2020/src/GUI/menu/Menu.java
@@ -0,0 +1,165 @@
+package GUI.menu;
+
+
+import GUI.Main;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Menu extends AnchorPane {
+
+    public ImageView fondMap;
+    public TextField choixJ;
+    public int nbPlayer;
+    int x,y = -1;
+
+
+    public void mouvFond() throws InterruptedException {
+        //Stoppper gauche
+        if(fondMap.getX() == 0){
+            x  =  -1;
+        }
+        //Stoppper droite
+        if(fondMap.getX() == -1542){
+            x  =  1;
+        }
+        //Stoppper haut
+        if(fondMap.getY() == 0){
+            y  =  -1;
+        }
+        //Stoppper bas
+        if(fondMap.getY() == -1662){
+            y  =  1;
+        }
+
+        fondMap.setX(fondMap.getX()+x);
+        fondMap.setY(fondMap.getY()+y);
+
+        //Dépassement
+        if(fondMap.getX() <= -1542){
+            fondMap.setX(-1542);
+        }
+        if(fondMap.getY() <= -1662){
+            fondMap.setY(-1662);
+        }
+    }
+
+    public boolean estUnEntier(String chaine) {
+        try {
+            Integer.parseInt(chaine);
+        } catch (NumberFormatException e){
+            return false;
+        }
+        return true;
+    }
+
+    public void lancerJeu() throws Exception {
+        System.out.println("le jeu se lance");
+        nbPlayer = Integer.parseInt( (choixJ.getCharacters()).toString());
+        System.out.println(nbPlayer);
+        /*
+        Main main = new Main();
+        Stage primaryStage = new Stage();
+        main.start(primaryStage,5);
+
+         */
+    }
+
+    public Menu()throws InterruptedException{
+        super();
+
+        //Fond
+        fondMap = new ImageView(new Image("Assets/DiceWarsMap.png"));
+
+        //selection du nombre de joueurs
+        choixJ = new TextField();
+        choixJ.setFont(Font.font(23));
+        choixJ.setMaxWidth(50.);
+
+        //Button lancement du jeu
+        Button b1 = new Button("Lancez le jeu");
+        b1.setOnMouseClicked(event -> {
+            try {
+                lancerJeu();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        b1.setDisable(true);
+
+        choixJ.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.equals("") || !estUnEntier(newValue)){
+                    b1.setDisable(true);
+                }
+                else{
+                    b1.setDisable(false);
+                }
+            }
+        });
+
+        Timeline fiveSecondsWonder = new Timeline(
+                new KeyFrame(Duration.seconds(0.04),
+                        new EventHandler<ActionEvent>() {
+
+                            @Override
+                            public void handle(ActionEvent event) {
+
+                                try {
+                                    mouvFond();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }));
+        fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
+        fiveSecondsWonder.play();
+
+
+
+        Text text = new Text("Saisir le nombre de joueur");
+        text.setFill(Color.GREEN);
+        text.setFont(Font.font(23));
+
+
+        //Ajout des éléments dans l'Anchor
+        getChildren().add(fondMap);
+        getChildren().add(b1);
+        getChildren().add(text);
+        getChildren().add(choixJ);
+
+        //Positionnement
+        AnchorPane.setLeftAnchor(b1,545.);
+        AnchorPane.setTopAnchor(b1,390.);
+        AnchorPane.setLeftAnchor(text,460.);
+        AnchorPane.setTopAnchor(text,270.);
+        AnchorPane.setLeftAnchor(choixJ,575.);
+        AnchorPane.setTopAnchor(choixJ,320.);
+
+        getStylesheets().add(this.getClass().getResource("../MenuStyle.css").toExternalForm());
+
+
+    }
+
+}
---
+++ Efrei2020/src/GUI/menu/testMenu.java
@@ -0,0 +1,32 @@
+package GUI.menu;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class testMenu extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Dice War");
+
+        Menu mainPane = new Menu();
+
+        //Canvas
+        //ImageView
+        //Text
+        //HBox, VBox ==> allignement des éléments
+        //ScrollPanel
+
+        primaryStage.setScene(new Scene(mainPane, 1200, 700));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
